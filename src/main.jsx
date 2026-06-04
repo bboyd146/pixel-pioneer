b65b123e5f0dc425e59ada114d8eee1d05305bf4
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2026-06-03 21:16:20 -0500

    incorporated react-router to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import WebsiteDesignHouston from "./pages/WebsiteDesignHouston";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route
+          path="/website-design-houston"
+          element={<WebsiteDesignHouston />}
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
